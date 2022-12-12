407eac8ac4d3760356b3264b6750f5ac06b1312b
nesb01t <littlelumos@foxmail.com>
2022-12-13 01:10:57 +0800

    Initial commit

---
+++ src/main/java/nesb01t/poweritemcraft/PowerItemCraft.java
@@ -0,0 +1,17 @@
+package nesb01t.poweritemcraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PowerItemCraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
