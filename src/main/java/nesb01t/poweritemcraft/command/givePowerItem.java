3ba164c21d6a69491aed78ba4c83cfe08e57d195
nesb01t <littlelumos@foxmail.com>
2022-12-13 20:20:44 +0800

    fix: command解耦

--- src/main/java/nesb01t/poweritemcraft/command/givePowerItem.java
+++ src/main/java/nesb01t/poweritemcraft/command/givePowerItem.java
@@ -1,8 +1,6 @@
 package nesb01t.poweritemcraft.command;
 
-import nesb01t.poweritemcraft.utils.ItemUtils;
 import nesb01t.poweritemcraft.utils.MessageUtils;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,8 +8,14 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import static nesb01t.poweritemcraft.core.items.ItemGenerator.createNewItem;
+import static nesb01t.poweritemcraft.utils.ItemUtils.getMaterialByName;
 
 public class givePowerItem implements CommandExecutor {
+    public static void givePowerItemToPlayer(String materialName, Player player) {
+        ItemStack poweritem = createNewItem(getMaterialByName(materialName));
+        player.getInventory().addItem(poweritem);
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player = (Player) sender;
@@ -19,9 +23,7 @@ public class givePowerItem implements CommandExecutor {
         MessageUtils.sendPlayerMsg(player, String.valueOf(args.length));
 
         if (args.length == 1) {
-            Material material = ItemUtils.getMaterialByName(args[0]);
-            ItemStack poweritem = createNewItem(material);
-            player.getInventory().addItem(poweritem);
+            givePowerItemToPlayer(args[0], player);
         }
         return true;
     }
